ba91d7616fc75df856faec2f390c40215a12c449
GUYOT-M <mallory.guyot@cpe.fr>
2020-11-05 15:39:23 +0100

    Correction warning size of

--- src/sizeof.c
+++ src/sizeof.c
@@ -2,15 +2,15 @@
 
 int main ()
 {
-    printf("taille du int : %d\n",sizeof (int));
-    printf("taille du int * :%d\n",sizeof (int *));
-    printf("taille du int ** :%d\n",sizeof (int **));
-    printf("taille du char * :%d\n",sizeof (char *));
-    printf("taille du char ** :%d\n",sizeof (char **));
-    printf("taille du char *** :%d\n",sizeof (char ***));
-    printf("taille du float * :%d\n",sizeof (float *));
-    printf("taille du float ** :%d\n",sizeof (float **));
-    printf("taille du float *** :%d\n",sizeof (float ***));
+    printf("taille du int : %lu\n",sizeof (int));
+    printf("taille du int * :%lu\n",sizeof (int *));
+    printf("taille du int ** :%lu\n",sizeof (int **));
+    printf("taille du char * :%lu\n",sizeof (char *));
+    printf("taille du char ** :%lu\n",sizeof (char **));
+    printf("taille du char *** :%lu\n",sizeof (char ***));
+    printf("taille du float * :%lu\n",sizeof (float *));
+    printf("taille du float ** :%lu\n",sizeof (float **));
+    printf("taille du float *** :%lu\n",sizeof (float ***));
     
     //la taille renvoyer à chaques fois est 4 car cela renvoie une adresse donc en 64bit est codé sur 4 octets
     
